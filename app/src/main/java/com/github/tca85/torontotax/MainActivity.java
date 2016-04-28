ed21309efc41058373023f0543f6b39d618f433c
tca85 <thiago.alves.85@gmail.com>
2016-04-28 19:47:13 -0400

    little changes

--- app/src/main/java/com/github/tca85/torontotax/MainActivity.java
+++ app/src/main/java/com/github/tca85/torontotax/MainActivity.java
@@ -1,12 +1,17 @@
 package com.github.tca85.torontotax;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+/**
+ * tca85
+ * <p/>
+ * Just an idiot app to calculate Toronto taxes
+ */
 public class MainActivity extends AppCompatActivity {
     private TextView txvTotal;
     private EditText edtPercentage;
@@ -30,8 +35,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 float percentage = Float.parseFloat(edtPercentage.getText().toString());
-                float dec = percentage / 100;
-                float total = Float.parseFloat(edtNumber.getText().toString()) + (dec * Float.parseFloat(edtNumber.getText().toString()));
+                float value = Float.parseFloat(edtNumber.getText().toString());
+
+                float total = value + ((percentage / 100) * value);
 
                 txvTotal.setText(Float.toString(total));
             }
